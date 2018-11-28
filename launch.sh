a4cbf57c8bd22a6dd3113aad0aebadac4dbd1e91
Dan Pipe-Mazo <dpipemazo@gmail.com>
2018-11-28 07:44:34 -0800

    launch: launch google voice helper automatically

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # TODO: launch your element and anything else needed here.
-echo "Hello, world!"
+python3 google/google_voice.py --credential google/credential.json --device_model_id elementary-211318-hulk-cue7ol
