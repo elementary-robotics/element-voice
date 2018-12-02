ae4f9f7a8a6dcbdd7a2093369ef39bcbb54c3c64
Dan Pipe-Mazo <dpipemazo@gmail.com>
2018-12-02 09:01:59 -0800

    sounds: still working on it

--- google/google_voice.py
+++ google/google_voice.py
@@ -15,9 +15,52 @@ from google.assistant.library import Assistant
 from google.assistant.library.event import EventType
 from google.assistant.library.file_helpers import existing_file
 
+from multiprocessing import Process
 import pygame
 
 from atom import Element
+
+class Sounds(object):
+    """
+    Class that plays all sounds
+    """
+    def __init__(self):
+        self.sounds = {}
+
+    def load_sound(self, name, filename):
+        self.sounds[name] = pygame.mixer.Sound(filename)
+
+    def play_sound(self, name):
+        if name in self.sounds:
+            self.sounds[name].play()
+            print ("Played sound {}".format(name))
+            return True
+        else:
+            print("Sound {} not supported!".format(name))
+            return False
+
+    def command_cb(self, data):
+        data_str = str(data).decode('ascii')
+        print("In commmand cb")
+        return "Success" if self.play_sound(data_str) else "Failure"
+
+def sound_thread():
+    """
+    Registers all of the sound playback commands for this element
+    and handles when they're called
+    """
+    pygame.init()
+
+    sound_element = Element("sound")
+
+    sounds = Sounds()
+    #sounds.load_sound("success", "/usr/local/share/sounds/success.mp3")
+    sounds.load_sound("fail", "/usr/local/share/sounds/fail.wav")
+
+    # Register our callback to play sounds
+    sound_element.command_add("play_sound", sounds.command_cb)
+    sound_element.command_loop()
+
 
 def process_event(event, assistant, element, sound_start):
     """
@@ -72,6 +115,12 @@ def main():
     # Set up the sound playback
     pygame.init()
     sound_start = pygame.mixer.Sound("/usr/local/share/sounds/on_conversation_start.wav")
+
+    # Set up the process that will play sounds for other processes. This
+    #   is needed until we get a shared PulseAudio system up and running
+    #   s.t. all elements can play their own sounds
+    sound_process = Process(target=sound_thread)
+    sound_process.start()
 
     with Assistant(credentials, args.device_model_id) as assistant:
 
