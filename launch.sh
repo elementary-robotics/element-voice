86eb7afd71f1769ae22ef565f588ca7d01c25780
Dan Pipe-Mazo <dpipemazo@gmail.com>
2019-03-30 06:52:28 -0700

    credential: remove from voice repo

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # TODO: launch your element and anything else needed here.
-python3 google/google_voice.py --credential google/credential.json --device_model_id elementary-211318-hulk-cue7ol
+python3 google/google_voice.py --credential google/credential.json --device_model_id ${DEVICE_MODEL_ID}
