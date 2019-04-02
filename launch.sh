b8f94c9dcb5188baea622913fcc444333f29404a
nambi-srivatsav <vatsa9myself@gmail.com>
2019-04-02 16:44:13 -0700

    README: Update to steps to include credentials
    Updated steps in README to include to Oauth 2.0 credentials and launch
    file to run with respective credentials

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # TODO: launch your element and anything else needed here.
-python3 google/google_voice.py --credential google/credential.json --device_model_id ${DEVICE_MODEL_ID}
+python3 google/google_voice.py --credential google/credentials.json --device_model_id ${DEVICE_MODEL_ID}
